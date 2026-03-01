5182b4a52ef83c4f7c0f83b9ac3b20c2991ccd62
Hafeez <syedhafeez957@gmail.com>
2026-03-01 23:31:49 +0530

    Navbar completed with responsive

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import MenuIcon from '@mui/icons-material/Menu';
+import ClearIcon from '@mui/icons-material/Clear';
+
+const Navbar = () => {
+  let [bool, setBool] = useState(true)
+
+  let toggle = () => {
+    setBool(!bool)
+  }
+  return (
+    < >
+      <nav className='bg-gray-300'>
+        <div id="navbar" className='flex justify-between p-7 pt-3 pb-3 md:justify-around p-3.5 bg-gray-400 rounded-4xl' >
+          <div id="logo" className={bool?'block':'hidden'}>
+            <h1 className='text-2xl text-blue-500'>Worker<span className='text-red-500'>Go</span></h1>
+          </div>
+          <div className="md:hidden">
+            <button onClick={toggle}>{bool ? <MenuIcon /> : <div className='relative left-65'><ClearIcon /></div>}</button>
+            {bool ? '' :
+
+              <div id="links" className='flex flex-col'>
+                <div id="logo" className='relative bottom-5'>
+                  <h1 className='text-2xl text-blue-500'>Worker<span className='text-red-500'>Go</span></h1>
+                </div>
+                <div className="">
+                  <ul className='flex flex-col  justify-around gap-5'>
+                    <li className='hover:bg-blue-500 rounded-xl p-1'><NavLink className=''>Home</NavLink></li>
+                    <li className='hover:bg-blue-500 rounded-xl p-1'><NavLink>Services</NavLink></li>
+                    <li className='hover:bg-blue-500 rounded-xl p-1'><NavLink>Login</NavLink></li>
+                    <li className='hover:bg-blue-500 rounded-xl p-1'><NavLink>Become a Worker</NavLink></li>
+                  </ul>
+                </div>
+              </div>
+            }
+          </div>
+
+          <div id="links" className='hidden md:block'>
+            <ul className='flex justify-around gap-5'>
+              <li className='hover:bg-blue-500 rounded-xl p-1'><NavLink className=''>Home</NavLink></li>
+              <li className='hover:bg-blue-500 rounded-xl p-1'><NavLink>Services</NavLink></li>
+              <li className='hover:bg-blue-500 rounded-xl p-1'><NavLink>Login</NavLink></li>
+              <li className='hover:bg-blue-500 rounded-xl p-1'><NavLink>Become a Worker</NavLink></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+    </ >
+  )
+}
+
+export default Navbar

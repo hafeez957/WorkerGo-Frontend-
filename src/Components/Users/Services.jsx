c99a839c49fc731149da0179adf7ba1a581e9711
Hafeez <syedhafeez957@gmail.com>
2026-03-03 23:17:13 +0530

    Implementing displaying data based on Categories

--- src/Components/Users/Services.jsx
+++ src/Components/Users/Services.jsx
@@ -3,10 +3,12 @@ import React, { useEffect, useState } from 'react'
 
 const Services = () => {
   let [servicesData,setServicesData]=useState([])
-    let handleData=async ()=>{
-        let data=await axios.get("http://localhost:3000/services")
-        setServicesData(data.data);
-        
+  let [CategoryData,SetCategoryData]=useState([])
+
+  let handleData=async ()=>{
+    let data=await axios.get("http://localhost:3000/services")
+    setServicesData(data.data); 
+    SetCategoryData(data.data);
     }
     useEffect(()=>{
       handleData()
@@ -14,12 +16,38 @@ const Services = () => {
     },[])
     console.log(servicesData);
 
+    let handleCategory=(e)=>{
+      let cat=e.target.value
+      let filtered_Data=servicesData.filter((ele)=>{
+        if(cat==="All"){
+          return CategoryData
+        }
+        return cat===ele.category
+      })
+      SetCategoryData(filtered_Data)
+    }
     
     
     return (
       < >
+      <div id="category" className=" bg-gray-200 flex justify-center ">
+        <select name="" id="" onChange={handleCategory} className='mt-2 p-1 border-2 rounded-2xl w-fit'>
+          <option value="">----Select Category----</option>
+          <option value="All">All</option>
+          <option value="Woodwork">Woodwork</option>
+          <option value="Cleaning">Cleaning</option>
+          <option value="Plumbing">Plumbing</option>
+          <option value="Electrical">Electrical</option>
+          <option value="Appliance">Appliance</option>
+          <option value="Home Improvement">Home Improvement</option>
+          <option value="Security">Security</option>
+          <option value="Outdoor">Outdoor</option>
+          <option value="Relocation">Relocation</option>
+          <option value="IT Services">IT Services</option>
+        </select>
+      </div>
         <div className=" grid gap-16 p-10 sm:grid-cols-2 lg:grid-cols-3 bg-gray-200">
-  {servicesData.map((ele) => {
+  {CategoryData.map((ele) => {
     const { id, category, name, description, image, price, rating } = ele;
 
     return (
